2208b83f74b723080ca3a1d7c53eaf20bab873dd
kupec <kupec-k@ya.ru>
2019-03-02 13:55:49 +0300

    Add js2r package

--- init.el
+++ init.el
@@ -9,6 +9,9 @@
 (global-set-key (kbd "C-x o") 'ace-window)
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "C-x g") 'magit-status)
+(js2r-add-keybindings-with-prefix "C-c C-m")
 
 (ido-mode)
 (company-mode)
+
+
